f72c84bcdc17074306687f006d1fd4be7dc29c5d
rbquick <quickrb@gmail.com>
2025-12-01 10:59:50 -0500

    added dateformater and watching search

---
+++ tvWatchedEmbedded/myViews/Settings.swift
@@ -0,0 +1,18 @@
+//
+//  Settings.swift
+//  tvWatchedEmbedded
+//
+//  Created by Brian Quick on 2025-11-22.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings()
+}
---
+++ tvWatchedEmbedded/myViews/myStyles/myDateFormatter.swift
@@ -0,0 +1,39 @@
+//
+//  myDateFormatter.swift
+//  HandicappIOS
+//
+//  Created by Brian Quick on 2021-11-14.
+//
+
+import SwiftUI
+
+func myDateFormatter(inDate: String, inFormat: String = "yyyy-MM-dd", outFormat: String = "yyyy-MM-dd") -> String {
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = inFormat
+    let mydate = inputFormatter.date(from: inDate) ?? Date()
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateFormat = outFormat
+    let outputDate =  outputFormatter.string(from: mydate)
+    return outputDate
+}
+func myDateFormatter(inDate: Date, outFormat: String = "yyyy-MM-dd") -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = outFormat
+    return dateFormatter.string(from: inDate as Date)
+}
+func myDateFormatter(inDate: Date, inFormat: String = "yyyy-MM-dd", outFormat: String = "yyyy-MM-dd") -> String {
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateFormat = outFormat
+    let outputDate =  outputFormatter.string(from: inDate)
+    return outputDate
+}
+func myDateIncrement(inDate: String, inFormat: String = "yyyy-MM-dd", outFormat: String = "yyyy-MM-dd") -> String {
+    let inputFormatter = DateFormatter()
+    inputFormatter.dateFormat = inFormat
+    let mydate = inputFormatter.date(from: inDate) ?? Date()
+    let incrementedDate = Calendar.current.date(byAdding: .day, value: 1, to: mydate)!
+    let outputFormatter = DateFormatter()
+    outputFormatter.dateFormat = outFormat
+    let outputDate =  outputFormatter.string(from: incrementedDate)
+    return outputDate
+}
